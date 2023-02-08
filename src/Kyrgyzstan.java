37d5bd927de33c168d07b80a344c8e070c5adc36
dolonovazarina <dolonovazarina87@gmail.com>
2023-02-08 20:29:10 +0300

    Initial commit

---
+++ src/Kyrgyzstan.java
@@ -0,0 +1,18 @@
+public enum Kyrgyzstan {
+
+    BATKEN("Kadamjai"),
+    ISSIKKOL("CHolpon Ata"),
+    JALALABAT("Kurmanbek"),
+    OSH(" Alai"),
+    TALAS("Manas"),
+    NAARIN ("Kochkor"),
+    CHUI(" Kemin");
+    private String name;
+
+    Kyrgyzstan(String name) {
+        this.name = name;
+    }
+     public void method(){
+         System.out.println("Bizdin mekenibiz");
+     }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      Kyrgyzstan[] kyrgyzstans=Kyrgyzstan.values();
+      for ( Kyrgyzstan kyrgyzstan: kyrgyzstans){
+          System.out.println(kyrgyzstan);
+      }
+    }
+}
